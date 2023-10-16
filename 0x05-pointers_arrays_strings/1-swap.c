3768b9389299474cc38c476bb56a8447505d052f
BahaaAhmedOfficial <BahaaAhmedOfficial@gmail.com>
2023-10-16 10:51:19 -0700

    bla

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * swap_int - swaps he cvaule of the two numbers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: Nothing
+ */
+
 void swap_int(int *a, int *b)
 {
 	int temp;
